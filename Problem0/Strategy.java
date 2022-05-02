33c2bbf43295c604feea49f9e40dd17f5cb1c4ab
Prosantodeb7 <prosantodeb7@gmail.com>
2022-05-02 08:28:02 +0600

    Strategy Pattern Added

---
+++ Problem0/Strategy.java
@@ -0,0 +1,7 @@
+
+package Problem0;
+
+public interface Strategy 
+{
+    public int doOperation(int a, int b);
+}
---
+++ Problem0/operationAdd.java
@@ -0,0 +1,11 @@
+
+
+package Problem0;
+
+public class operationAdd implements Strategy
+{
+    @Override
+    public int doOperation(int a, int b){
+         return a+b;
+    }
+}
